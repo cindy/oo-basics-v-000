61c629ca4097d10f051893d144d33aad07fc07e7
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-16 18:23:52 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,5 @@
 class Book
-<<<<<<< HEAD
-
   attr_accessor :title, :author, :page_count, :genre
-=======
-  def initialize(title)
-    @title = title 
-  end
-end
->>>>>>> bd6b15ccce4e4ef5c3e6208cb9305e665f107187
 
   def initialize(title)
     @title = title
